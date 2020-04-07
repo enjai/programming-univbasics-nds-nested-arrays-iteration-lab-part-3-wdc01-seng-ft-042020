b8a7c6b78bbc25f74f5fb12aee0dac78570dfa0e
Injae Lee <ilee.injae@gmail.com>
2020-04-07 16:24:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,4 +15,19 @@ end
 sentence
 end
 
-def 
+def join_nested_strings(src)
+  final_string = ""
+  row_index = 0 
+  while row_index < src.count do 
+    element_index = 0 
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index].class == string 
+        final_string += src[row_index][element_index] + ' '
+      end 
+      element_index += 1 
+    end 
+    row_index += 1 
+  end 
+  final_string 
+end 
+
